ab7f2b9771b969e972b168d8b95b0aa08acc3398
astur <astur@yandex.ru>
2017-10-29 23:26:24 +0300

    First implementation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = {};
+const deepFreeze = require('get-props');
+
+module.exports = o => {
+    const frozen = new Set();
+
+    const freeze = o => {
+        Object.freeze(o);
+        frozen.add(o);
+        const isF = typeof o === 'function';
+        deepFreeze(o, {protos: true, symbols: true})
+            .filter(v => !isF || !['caller', 'callee', 'arguments'].includes(v))
+            .filter(v => o[v] !== null)
+            .filter(v => ['object', 'function'].includes(typeof o[v]))
+            .filter(v => !ArrayBuffer.isView(o[v]))
+            .filter(v => !frozen.has(o[v]))
+            .forEach(v => freeze(o[v]));
+    };
+
+    freeze(o);
+    return o;
+};
